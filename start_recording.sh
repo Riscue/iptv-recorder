b2b636a8a110c8cb79d4643ea5d7091ecc3d38d1
İbrahim Akyel <ibrahim@ibrahimakyel.com>
2023-10-17 15:26:07 +0300

    Change paths

--- start_recording.sh
+++ start_recording.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-croncmd="/home/pi/iptv_recorder/start_recording.sh"
+croncmd="/home/pi/raspberry.pi/services/iptv-recorder/start_recording.sh"
 cronenv="START_DATE=\"$START_DATE\" FINISH_DATE=\"$FINISH_DATE\" CHANNEL_NAME=\"$CHANNEL_NAME\" CHANNEL_URL=\"$CHANNEL_URL\""
 
 ( crontab -l | grep -v -F "$cronenv $croncmd" ) | crontab -
@@ -10,4 +10,6 @@ FILE_PATH="/mnt/media/IPTV_Records"
 
 ffmpeg -i "$CHANNEL_URL" -c copy "$FILE_PATH/$FILE_NAME.mp4" &
 
-echo "$(date +%Y-%m-%d-%H-%M-%S) - Started for $CHANNEL_NAME" >> /home/pi/iptv_recorder/cron.log
+mkdir -p /var/log/iptv-recorder/
+
+echo "$(date +%Y-%m-%d-%H-%M-%S) - Started for $CHANNEL_NAME" >> /var/log/iptv-recorder/cron.log
