61847833d664f7b0321f05f1e0cbd83e52d7b7b8
İbrahim Akyel <ibrahim@ibrahimakyel.com>
2023-11-01 20:56:50 +0300

    Log date

--- src/log-controller.js
+++ src/log-controller.js
@@ -1,7 +1,7 @@
 module.exports = class LogController {
 
     static info(entity, action, object) {
-        console.log(JSON.stringify({entity, action, object, severity: "info"}));
+        console.log(JSON.stringify({timestamp: new Date(), entity, action, object, severity: "info"}));
     }
 
     static error(entity, action, errorLog) {
