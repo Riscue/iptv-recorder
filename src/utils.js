c8cc01de706801c2b54aa1b1a749232a41e4dba2
İbrahim Akyel <ibrahim@ibrahimakyel.com>
2023-11-02 14:27:48 +0300

    Fix some

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,6 @@ const {downloadFolder} = require("./contants");
 const moment = require("moment");
 module.exports = {
     getFileName: function (title) {
-        return `${downloadFolder}/${title.trim().replace(/[ \/\\:]+/g, '-')}-${moment(new Date()).format('yyyy-MM-DD-HH-mm')}.mp4`;
+        return `${downloadFolder}/${title.trim().replace(/[ \/\\:]+/g, '-')}-${moment(new Date()).format('yyyy-MM-DD-HH-mm-ss')}.mp4`;
     }
 }
